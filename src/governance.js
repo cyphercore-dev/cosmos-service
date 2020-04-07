ff17499181def15258402a7c6825a5d93f8add33
aakatev <aakatev@mail.sfsu.edu>
2020-04-07 09:37:15 -0700

    Governance module completed

--- src/governance.js
+++ src/governance.js
@@ -15,8 +15,36 @@ function deposits(id, depositor = null) {
   return http.get(`/gov/proposals/${id}/deposits`)
 }
 
+function votes(id, voter = null) {
+  if(voter) {
+    return http.get(`/gov/proposals/${id}/votes/voter`)  
+  }
+  return http.get(`/gov/proposals/${id}/votes`)
+}
+
+function tally(id) {
+  return http.get(`/gov/proposals/${id}/tally`)
+}
+
+function deposit() {
+  return http.get(`/gov/parameters/deposit`)
+}
+
+function tallying() {
+  return http.get(`/gov/parameters/tallying`)
+}
+
+function voting() {
+  return http.get(`/gov/parameters/voting`)
+}
+
 module.exports = {
   proposals,
   proposer,
-  deposits
+  deposits,
+  votes,
+  tally,
+  deposit,
+  tallying,
+  voting
 }
